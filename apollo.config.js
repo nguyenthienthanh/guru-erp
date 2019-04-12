dc36c4ef72e9de2b4d53681bade130041468cadd
Nguyễn Thiện Thanh <nguyenthienthanh.kiyoshi@gmail.com>
2019-04-12 11:46:28 +0700

    feat: add vscode apollo plugin configuration (#135)
    Closes #48

---
+++ apollo.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'guru-erp',
+      localSchemaFile: './packages/server/src/services/gateway/generated-schema.gql',
+    },
+    includes: [
+      'packages/server/src/**/__test__/**/*.{ts,gql,graphql}',
+      'packages/web-app/src/**/*.{ts,gql,graphql}',
+      'packages/desktop-app/src/**/*.{ts,gql,graphql}',
+    ],
+    excludes: ['node_modules/**/*'],
+  },
+}
